2e2812f945a92209298453678540e59463a55034
Eduardo Silva <eduardo@eth0.com.br>
2024-08-06 20:05:18 -0300

    basic authentication

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, Response
 from dotenv import load_dotenv
 import os
 import paramiko
@@ -16,6 +16,8 @@ SCRIPT_WAN2 = os.getenv('SCRIPT_WAN2')
 SCRIPT_TOGGLE = os.getenv('SCRIPT_TOGGLE')
 SCRIPT_CHECK = os.getenv('SCRIPT_CHECK')
 HTML_REFRESH = int(os.getenv('HTML_REFRESH', 0))
+WEB_USER = os.getenv('WEB_USER')
+WEB_PASSWORD = os.getenv('WEB_PASSWORD', '')
 
 ROUTER_IP = os.getenv('ROUTER_IP')
 ROUTER_USER = os.getenv('ROUTER_USER')
@@ -39,6 +41,7 @@ def execute_script(script_name):
     except Exception as e:
         return str(e)
 
+
 def check_current_isp():
     output = execute_script(SCRIPT_CHECK)
     if '%' in output:
@@ -49,11 +52,32 @@ def check_current_isp():
             return WAN2_NAME
     return output
 
+
+def check_auth(username, password):
+    return username == WEB_USER and password == WEB_PASSWORD
+
+
+def authenticate():
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+@app.before_request
+def require_auth():
+    if WEB_USER and WEB_PASSWORD is not None:  # Verifica se WEB_USER está definido e WEB_PASSWORD pode ser vazio
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+
+
 @app.route('/')
 def index():
     current_isp = check_current_isp()
     error = request.args.get('error')
     return render_template('index.html', isp=current_isp, wan1_name=WAN1_NAME, wan2_name=WAN2_NAME, error=error, refresh=HTML_REFRESH)
+
 
 @app.route('/toggle')
 def toggle_isp():
@@ -61,6 +85,7 @@ def toggle_isp():
     if error:
         return redirect(url_for('index', error=error))
     return redirect(url_for('index'))
+
 
 @app.route('/set/<wan>')
 def set_isp(wan):
@@ -73,6 +98,7 @@ def set_isp(wan):
         if error:
             return redirect(url_for('index', error=error))
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
